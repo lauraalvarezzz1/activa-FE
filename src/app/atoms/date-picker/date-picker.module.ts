5e8c6ac0f48002b481a52606995081c977373fa5
Laura Cristina Alvarez <lauraalvarez.mgvc@gmail.com>
2020-02-04 11:47:34 -0500

    adding atoms

---
+++ src/app/atoms/date-picker/date-picker.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DatePickerComponent } from './date-picker.component';
+
+@NgModule({
+    declarations: [
+        DatePickerComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class DatePickerModule{}
---
+++ src/app/atoms/input-text/input-text.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextComponent } from './input-text.component';
+
+@NgModule({
+    declarations: [
+        InputTextComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class InputTextModule{}
